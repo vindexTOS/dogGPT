7c13ef9cc90fe95433a575fedb82c4ef7c9acb68
VindexTOS <giorgikutateladze1998@gmail.com>
2023-03-11 01:07:55 +0400

    header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 function Header(props) {
   return (
     <header className="w-[100vw] flex flex-col items-center justify-center border-b-2 border-gray-600">
-      <h1 className="text-[2rem] text-white font-bold">WingoGPT</h1>
+      <h1 className="text-[2rem] text-white font-bold">DogGPT</h1>
       <div className="flex  items-cneter  justify-center">
         <p className="text-white">
-          Keep in mind i do not joke around, so say something constructive
+          Ask Me Something And Ill Tell You Cool Dog Fact
         </p>
       </div>
     </header>
